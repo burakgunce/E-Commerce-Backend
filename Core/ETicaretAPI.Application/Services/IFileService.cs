3aef508fc3f5a3e851a84d17fab95ab886fa9f62
burakgunce <brk.gnc9@hotmail.com>
2024-02-23 02:41:41 +0300

    file service has completed

--- Core/ETicaretAPI.Application/Services/IFileService.cs
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -10,7 +10,7 @@ namespace ETicaretAPI.Application.Services
     public interface IFileService
     {
         Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files);
-        Task<string> FileRenameAsync(string fileName);
+        //Task<string> FileRenameAsync(string fileName);
         Task<bool> CopyFileAsync(string path, IFormFile file);
     }
 }
