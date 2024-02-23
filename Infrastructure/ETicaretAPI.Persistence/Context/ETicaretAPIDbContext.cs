67acabea37839af24b8b3dc828576773aafe8b46
burakgunce <brk.gnc9@hotmail.com>
2024-02-23 23:33:40 +0300

    control mechanism for files has been added

--- Infrastructure/ETicaretAPI.Persistence/Context/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Context/ETicaretAPIDbContext.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using File = ETicaretAPI.Domain.Entities.File;
 
 namespace ETicaretAPI.Persistence.Context
 {
@@ -18,6 +19,9 @@ namespace ETicaretAPI.Persistence.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<ProductImageFile> ProductImageFiles { get; set; }
+        public DbSet<InvoiceFile> InvoiceFiles { get; set; }
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
