5da0cf43f15e3cbb9db01cdd6bfe5be2104f0fd9
burakgunce <brk.gnc9@hotmail.com>
2024-02-25 23:50:55 +0300

    storage services mechanism has been updated

--- Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
@@ -1,5 +1,7 @@
 ﻿
+using ETicaretAPI.Application.Abstractions.Storage;
 using ETicaretAPI.Infrastructure.Services;
+using ETicaretAPI.Infrastructure.Services.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,7 +15,11 @@ namespace ETicaretAPI.Infrastructure
     {
         public static void AddInfrastructureServices(this IServiceCollection serviceCollection)
         {
-            
+            serviceCollection.AddScoped<IStorageService, StorageService>();
+        }
+        public static void AddStorage<T>(this IServiceCollection serviceCollection) where T : class, IStorage
+        {
+            serviceCollection.AddScoped<IStorage, T>();
         }
     }
 }
