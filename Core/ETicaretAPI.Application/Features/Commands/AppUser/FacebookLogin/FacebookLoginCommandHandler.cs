bbe240c4edf9877477abace49bd392458f6039f4
burakgunce <brk.gnc9@hotmail.com>
2024-03-05 00:48:48 +0300

    facebook login mechanism has been added

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -0,0 +1,78 @@
+﻿using ETicaretAPI.Application.Abstractions.Token;
+using ETicaretAPI.Application.DTOs;
+using ETicaretAPI.Application.DTOs.Facebook;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
+{
+    public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly ITokenHandler _tokenHandler;
+        readonly HttpClient _httpClient;
+
+        public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, IHttpClientFactory httpClientFactory)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            string accessTokenResponse = await _httpClient.GetStringAsync("https:// link + facebook client id, client secret, client credential");
+
+            FacebookAccessTokenResponse_DTO facebookAccessTokenResponse = JsonSerializer.Deserialize<FacebookAccessTokenResponse_DTO>(accessTokenResponse);
+
+            string userAccessTokenValidation = await _httpClient.GetStringAsync("https// link + input_token {request.AuthToken}, access_token {facebookAccessTokenResponse.AccessToken}");
+
+            FacebookUserAccessTokenValidation_DTO validation = JsonSerializer.Deserialize<FacebookUserAccessTokenValidation_DTO>(userAccessTokenValidation);
+
+            if (validation.Data.IsValid)
+            {
+                string userInfoResponse = await _httpClient.GetStringAsync("https link + me? + fields email,name & access_token {request.AuthToken}");
+
+                FacebookUserInfoResponse userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+
+                var info = new UserLoginInfo("FACEBOOK", validation.Data.UserId, "FACEBOOK");
+
+                Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                bool result = user != null;
+                if (user == null)
+                {
+                    user = await _userManager.FindByEmailAsync(userInfo.Email);
+                    if (user == null)
+                    {
+                        user = new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Email = userInfo.Email,
+                            UserName = userInfo.Email,
+                            NameSurname = userInfo.Name
+                        };
+                        var identityResult = await _userManager.CreateAsync(user);
+                        result = identityResult.Succeeded;
+                    }
+                }
+
+                if (result)
+                {
+                    await _userManager.AddLoginAsync(user, info); // AspNetUserLogins Table
+
+                    Token token = _tokenHandler.CreateAccessToken(5);
+                    return new()
+                    {
+                        Token = token
+                    };
+                }
+                    
+            }
+            throw new Exception("Invalid external authentication");
+
+        }
+    }
+
+}
--- ETicaretAPI.API/Controllers/UsersController.cs
+++ ETicaretAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
+using ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin;
 using ETicaretAPI.Application.Features.Commands.AppUser.GoogleLogin;
 using ETicaretAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
@@ -38,5 +39,14 @@ namespace ETicaretAPI.API.Controllers
             GoogleLoginCommandResponse response = await _mediator.Send(googleLoginCommandRequest);
             return Ok(response);
         }
+
+        [HttpPost("facebook-login")]
+        public async Task<IActionResult> FacebookLogin(FacebookLoginCommandRequest facebookLoginCommandRequest)
+        {
+            FacebookLoginCommandResponse response = await _mediator.Send(facebookLoginCommandRequest);
+            return Ok(response);
+        }
+
+
     }
 }
