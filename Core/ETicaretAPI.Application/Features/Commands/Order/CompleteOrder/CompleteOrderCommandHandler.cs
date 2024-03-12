cf081c323ba2006635d30d4f0d4a1ed18370dc24
burakgunce <brk.gnc9@hotmail.com>
2024-03-12 18:53:44 +0300

    function that needs for mailing for completed orders has been added

--- Core/ETicaretAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Application.DTOs.Order;
 using MediatR;
 
 namespace ETicaretAPI.Application.Features.Commands.Order.CompleteOrder
@@ -6,15 +7,19 @@ namespace ETicaretAPI.Application.Features.Commands.Order.CompleteOrder
     public class CompleteOrderCommandHandler : IRequestHandler<CompleteOrderCommandRequest, CompleteOrderCommandResponse>
     {
         IOrderService _orderService;
+        IMailService _mailService;
 
-        public CompleteOrderCommandHandler(IOrderService orderService)
+        public CompleteOrderCommandHandler(IOrderService orderService, IMailService mailService)
         {
             _orderService = orderService;
+            _mailService = mailService;
         }
 
         public async Task<CompleteOrderCommandResponse> Handle(CompleteOrderCommandRequest request, CancellationToken cancellationToken)
         {
-            await _orderService.CompleteOrderAsync(request.Id);
+            (bool succeeded, CompletedOrderDTO dto) = await _orderService.CompleteOrderAsync(request.Id);
+            if (succeeded)
+                await _mailService.SendCompletedOrderMailAsync(dto.Email, dto.OrderCode, dto.OrderDate, dto.Username);
             return new();
         }
     }
