42ef155df4b15fe4bf091809c81fd798f1eb33e0
burakgunce <brk.gnc9@hotmail.com>
2024-03-16 19:18:22 +0300

    services that update stock information of products via QR code have been added

---
+++ Core/ETicaretAPI.Application/Features/Commands/Product/UpdateStockQrCodeToProduct/UpdateStockQrCodeToProductCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+
+namespace ETicaretAPI.Application.Features.Commands.Product.UpdateStockQrCodeToProduct
+{
+    public class UpdateStockQrCodeToProductCommandHandler : IRequestHandler<UpdateStockQrCodeToProductCommandRequest, UpdateStockQrCodeToProductCommandResponse>
+    {
+        readonly IProductService _productService;
+
+        public UpdateStockQrCodeToProductCommandHandler(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<UpdateStockQrCodeToProductCommandResponse> Handle(UpdateStockQrCodeToProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _productService.StockUpdateToProductAsync(request.ProductId, request.Stock);
+            return new();
+        }
+    }
+
+}
